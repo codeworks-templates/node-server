5f3164b88c4aada618e0f8ff86d09fd32516afd5
Jake Overall <joverall22@gmail.com>
2020-02-25 17:26:25 -0700

    Add files via upload

---
+++ server/controllers/ValuesController.js
@@ -0,0 +1,27 @@
+import express from "express";
+import BaseController from "../utils/BaseController";
+import { valuesService } from "../services/ValueService";
+
+export class ValuesController extends BaseController {
+  constructor() {
+    super("api/values");
+    this.router = express
+      .Router()
+      .get("", this.getAll)
+      .post("", this.create);
+  }
+  async getAll(_, res, next) {
+    try {
+      return res.send(["value1", "value2"]);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async create(req, res, next) {
+    try {
+      res.send(req.body);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
